5b1babdf7621ec76d6e20f072a734c5971ef3b23
cgrevil <cgrevil@gmail.com>
2017-05-08 16:37:53 +0200

    Negation added

--- inference.pl
+++ inference.pl
@@ -1,15 +1,13 @@
+/* Knowledge base types */
+isclass(X) :- kb(X,_,_).
+isclass(X) :- kb(_,_,X).
+isrelation(R) :- kb(_,R,_).
+
 /* isa relationship */
 isa(X,X).
 isa(X,Y) :- kb(X,isa,Z),isa(Z,Y).
 overlap(X,Y) :- isa(Z,X), isa(Z,Y).
-disjoint(X,Y) :- overlap(X,Y),!,fail.
-disjoint(_,_).
-
-relates(a,X,isa,Y) :- isa(X,Y).
-relates(aa,X,R,Y) :- aa(X,R,Y).
-relates(ae,X,R,Y) :- ae(X,R,Y).
-relates(ea,X,R,Y) :- ea(X,R,Y).
-relates(ee,X,R,Y) :- ee(X,R,Y).
+disjoint(X,Y) :- isclass(X),isclass(Y),\+ overlap(X,Y).
 
 /* haspart relationship */
 ae(X,haspart,Y) :- kb(X,haspart,Y).
@@ -20,18 +18,19 @@ ae(X,secretes,Y) :- kb(X,secretes,Y).
 
 /* Relationship inheritance */
 ae(CP,R,D) :- kb(CP,isa,C), ae(C,R,D).
-ea(C,R,DP) :- kb(DP,isa,D), ea(C,R,D).
-aa(CP,R,D) :- kb(CP,isa,C), aa(C,R,D).
-aa(C,R,DP) :- kb(DP,isa,D), aa(C,R,D).
 
 /* Relationship generalization */
 ae(C,R,DP) :- kb(D,isa,DP), ae(C,R,D).
 ee(CP,R,D) :- kb(C,isa,CP), ee(C,R,D).
-ee(C,R,DP) :- kb(D,isa,DP), ae(C,R,D).
-ea(CP,R,D) :- kb(C,isa,CP), ea(C,R,D).
 
 /* Weakening of quantifier */
-ae(C,R,D) :- aa(C,R,D).
-ea(C,R,D) :- aa(C,R,D).
 ee(C,R,D) :- ae(C,R,D).
-ee(C,R,D) :- ea(C,R,D).
+
+/* Meta level relationships */
+relates(every,X,isa,Y) :- isa(X,Y).
+relates(some,X,isa,Y) :- overlap(X,Y).
+relates(no,X,isa,Y) :- disjoint(X,Y).
+
+relates(every,X,R,Y) :- ae(X,R,Y).
+relates(some,X,R,Y) :- ee(X,R,Y).
+relates(no,X,R,Y) :- isclass(X),isclass(Y),isrelation(R), \+ relates(some,X,R,Y).
--- knowledgebase.pl
+++ knowledgebase.pl
@@ -7,7 +7,7 @@ kb(neuronal_schwann_cell,isa,cell).
 kb(exocrine_pancreatic_cell,isa,exocrine_cell).
 kb(exocrine_pancreatic_cell,isa,pancreatic_cell).
 kb(endocrine_pancreatic_cell,isa,pancreatic_cell).
-kb(endocrine_cell,isa,endocrine_pancreatic_cell).
+kb(endocrine_pancreatic_cell,isa,endocrine_cell).
 kb(adult_stem_cell,isa,stem_cell).
 kb(embryonic_stem_cell,isa,stem_cell).
 kb(alpha_cell,isa,endocrine_pancreatic_cell).
@@ -16,6 +16,14 @@ kb(omega_cell,isa,endocrine_pancreatic_cell).
 kb(pp_cell,isa,endocrine_pancreatic_cell).
 kb(centroacinar_cell,isa,exocrine_pancreatic_cell).
 kb(acinar_cell,isa,exocrine_pancreatic_cell).
+
+kb(glucagon,isa,hormone).
+kb(insulin,isa,hormone).
+kb(gastrin,isa,hormone).
+kb(somatostatin,isa,hormone).
+kb(pancreatic_polypeptide,isa,hormone).
+kb(digestive_enzymes,isa,enzyme).
+kb(acinar_cell,isa,enzyme).
 
 /* Part of */
 kb(nervous_system,haspart,neuronal_schwann_cell).
@@ -36,3 +44,5 @@ kb(beta_cell,secretes,insulin).
 kb(omega_cell,secretes,gastrin).
 kb(omega_cell,secretes,somatostatin).
 kb(pp_cell,secretes,pancreatic_polypeptide).
+kb(centroacinar_cell,secretes,digestive_enzymes).
+kb(acinar_cell,secretes,bicarbonate_ions).
